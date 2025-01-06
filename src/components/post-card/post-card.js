55f7e7d495dd6d2c6701de2547637992ee978e2d
elisdotenv <elisdotenv@gmail.com>
2025-01-06 10:37:56 +0300

    Initial Commit (PostCard Component)

---
+++ src/components/post-card/post-card.js
@@ -0,0 +1,14 @@
+const PostCard = () => {
+  const Title = `This is some random title from the world!`;
+  const Brief = `Some random brief summary goes on here explaining some random junky shit and shut!`;
+  return (
+    <>
+      <div className={`p-[2rem] border-[1.5px] border-purple-800 rounded-lg`}>
+        <h1>Title: {Title}</h1>
+        <p>Brief: {Brief}</p>
+      </div>
+    </>
+  );
+};
+
+export default PostCard;
